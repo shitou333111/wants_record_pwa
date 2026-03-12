50b6e8f88a1a2de622e52a4a11356dba4a936e17
User <user@example.com>
2026-03-12 16:30:06 +0800

    add baidu statistics [release]

--- src/HelpPage.tsx
+++ src/HelpPage.tsx
@@ -230,10 +230,10 @@ const HelpPage: React.FC<HelpPageProps> = ({ onExport, onImportFile, onClearData
             <p className="help-row-text"><strong>样式美化</strong>：⚡ 面板动态背景、通知栏融合、首页按钮颜色、更美观的字体。</p>
           </div>
           <div className="help-row">
-            <p className="help-row-text">补充更多资料</p>
+            <p className="help-row-text"><strong>功能更新</strong>：释放练习加入音频提示、补充更多资料</p>
           </div>
           <div className="help-row">
-            <p className="help-row-text"><a href="https://wj.qq.com/s2/25934282/dfce/" target="_blank" rel="noopener noreferrer external" referrerPolicy="no-referrer">点击这里</a>或扫码，反馈Bug和需求  <img src="/feedback.png" alt="腾讯问卷扫码" className="help-feedback-Qcode" onClick={() => setShowFeedbackImg(true)} /> </p>
+            <p className="help-row-text"><a href="https://wj.qq.com/s2/25934282/dfce/" target="_blank" rel="noopener noreferrer" referrerPolicy="no-referrer">点击这里</a>或扫码，反馈Bug和需求  <img src="/feedback.png" alt="腾讯问卷扫码" className="help-feedback-Qcode" onClick={() => setShowFeedbackImg(true)} /> </p>
           </div>
         </div>
       </div>
